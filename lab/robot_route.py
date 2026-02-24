543fee9c0007b33bcc928066884f1c31e8d69df9
BMaksymV <101249589+BMaksymV@users.noreply.github.com>
2026-02-24 21:05:21 +0200

    cleaned useless code from robot_route.py

--- lab/robot_route.py
+++ lab/robot_route.py
@@ -23,23 +23,3 @@ x = [
     ]
 
 print(robot_route(x))
-
-def robot_route_reversed(matrix):
-    route = []
-    if matrix:
-        for m in reversed(range(len(matrix))): 
-            if m % 2 == 0:
-                route.extend(matrix[m])
-            else:
-                route.extend(matrix[m][::-1])
-            
-    return route
-
-x = [
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10],
-    [11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20],
-]
-
-print(robot_route_reversed(x))
